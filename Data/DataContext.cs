8f2daffd3a0b0d1a663c432157946e251028ade7
rodercode <rodersnow@gmail.com>
2023-11-25 07:50:48 +0100

    Generate Database, Table and connection
    These are the things been done
    - Create Pokemon database
    - Add a connection string to database
    - Create a DataContext for communcation to db
    - Create a Pokemon Domain model
    - add pokemon dbset in DataContext
    - Add migrations Initial-create
    - config connection to a sql server

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using pokeapi.Models;
+
+namespace pokeapi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Pokemon> Pokemons { get; set; }        
+    }
+}
---
+++ Models/Pokemon.cs
@@ -0,0 +1,6 @@
+namespace pokeapi.Models;
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
