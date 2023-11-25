d627e76cee1be6032274b765fd3c5f35d712fa6b
rodercode <rodersnow@gmail.com>
2023-11-25 06:55:25 +0100

    Introduce generic model
    An generic model to handle api response

---
+++ Models/ApiResponse.cs
@@ -0,0 +1,9 @@
+namespace pokeapi.Models
+{
+    public class ApiResponse<T>
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; } = "";
+        public T? Data { get; set; }
+    }
+}
